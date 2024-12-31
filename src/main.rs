665c30d60136d7856ccd62f5f6f2f4c5d6000cc3
Jochen Stierberger <jocsti@web.de>
2024-12-31 08:36:26 -0600

    feat: Run cargo sqlx prepare

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 use sqlx::PgPool;
-use tokio::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup::app};
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres");
-    let address = format!("127.0.0.1:{}", &configuration.application_port);
-    // let listener = tokio::net::TcpListener::bind("127.0.0.1:{}").await.unwrap();
-    let listener = TcpListener::bind(address).await?;
     axum::serve(listener, app(connection_pool)).await.unwrap();
-
-    Ok(())
 }
