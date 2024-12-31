8ca531a1c23359cb49619589c53be4c1d4c6c6c7
Jochen Stierberger <jocsti@web.de>
2024-12-31 07:51:04 -0600

    feat: Bind the port

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use sqlx::PgPool;
+use tokio::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup::app};
 
 #[tokio::main]
-async fn main() {
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
+async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres");
+    let address = format!("127.0.0.1:{}", &configuration.application_port);
+    // let listener = tokio::net::TcpListener::bind("127.0.0.1:{}").await.unwrap();
+    let listener = TcpListener::bind(address).await?;
     axum::serve(listener, app(connection_pool)).await.unwrap();
+
+    Ok(())
 }
