e4c362c9a07e4f08ef2ad14d56acb1882710eb19
Jochen Stierberger <jocsti@web.de>
2025-01-07 07:23:00 -0600

    feat: finish 6.1

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 Working on the book Zero 2 Production with Axum.
 
-Learned:
+## Progress
+- [x] Chapter 1
+- [x] Chapter 2
+- [x] Chapter 3
+- [x] Chapter 4
+- [x] Chapter 5
+- [] Chapter 6
+
+## Learned
+### Different status code returned
+Axum returns `StatusCode::UNPROCESSABLE_ENTITY` whereas actix-web `StatusCode::BAD_REQUEST` when a handler can't process the body of an incoming request.
+
 ### `error: error with configuration: relative URL without a base` when running `sqlx migrate run`
 This happened at the end of chapter 5 when we tried to migrate the new database URL. I forgot to wrap that URL in double quotes.
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -26,6 +26,47 @@ static TRACING: Lazy<()> = Lazy::new(|| {
     }
 });
 
+async fn spawn_app() -> (SocketAddr, PgPool) {
+    Lazy::force(&TRACING);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let addr = listener.local_addr().unwrap();
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let connection_pool = configure_database(&configuration.database).await;
+    let server_pool = connection_pool.clone();
+
+    let _ = tokio::spawn(async move {
+        axum::serve(listener, zero2prod::startup::app(server_pool))
+            .await
+            .unwrap();
+    });
+    (addr, connection_pool)
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // Create database
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    // Migrate database
+    let connection_pool = PgPool::connect_with(config.with_db())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
+}
+
 #[tokio::test]
 async fn health_check_oneshot() {
     let mut configuration = get_configuration().expect("Failed to read configuration.");
@@ -125,43 +166,36 @@ async fn subscribe_returns_a_422_when_data_is_missing() {
     }
 }
 
-async fn spawn_app() -> (SocketAddr, PgPool) {
-    Lazy::force(&TRACING);
+#[tokio::test]
+async fn subscribe_returns_a_200_when_fields_are_present_but_missing() {
+    // Arrange
+    let (app_address, _) = spawn_app().await;
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
-    let addr = listener.local_addr().unwrap();
-    let mut configuration = get_configuration().expect("Failed to read configuration.");
-    configuration.database.database_name = Uuid::new_v4().to_string();
+    let test_cases = vec![
+        ("name=&email=james@x.com", "this should have a name"),
+        ("name=Jenny&email=", "missing the email"),
+        ("name=Bob&email=yes@%", "corrupted email"),
+    ];
+    let client = reqwest::Client::new();
 
-    let connection_pool = configure_database(&configuration.database).await;
-    let server_pool = connection_pool.clone();
-
-    let _ = tokio::spawn(async move {
-        axum::serve(listener, zero2prod::startup::app(server_pool))
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(format!("http://{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
             .await
-            .unwrap();
-    });
-    (addr, connection_pool)
-}
+            .expect("Failed to execute request.");
+        // Assert
+        assert_eq!(
+            StatusCode::OK,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 
-pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    // Create database
-    let mut connection = PgConnection::connect_with(&config.without_db())
-        .await
-        .expect("Failed to connect to Postgres");
-    connection
-        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
-        .await
-        .expect("Failed to create database.");
-
-    // Migrate database
-    let connection_pool = PgPool::connect_with(config.with_db())
-        .await
-        .expect("Failed to connect to Postgres.");
-    sqlx::migrate!("./migrations")
-        .run(&connection_pool)
-        .await
-        .expect("Failed to migrate the database");
-
-    connection_pool
+    //TODO: Delete created database
 }
