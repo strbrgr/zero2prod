f9a153743020fe69c5e3fb428e43e8b8d31f36d7
Jochen Stierberger <jocsti@web.de>
2025-01-08 06:24:58 -0600

    feat: finish 6.14.0

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -22,8 +22,20 @@ impl AsRef<str> for SubscriberEmail {
 #[cfg(test)]
 mod tests {
     use super::SubscriberEmail;
-    use claims::{assert_err, assert_ok};
+    use claims::assert_err;
     use fake::{faker::internet::en::SafeEmail, Fake};
+    use rand::{rngs::StdRng, SeedableRng};
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            Self(email)
+        }
+    }
 
     #[test]
     fn empty_string_is_rejected() {
@@ -43,9 +55,8 @@ mod tests {
         assert_err!(SubscriberEmail::parse(email));
     }
 
-    #[test]
-    fn valid_emails_are_parsed_succesfully() {
-        let email = SafeEmail().fake();
-        assert_ok!(SubscriberEmail::parse(email));
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_succesfully(valid_email: ValidEmailFixture) {
+        (SubscriberEmail::parse(valid_email.0).is_ok());
     }
 }
